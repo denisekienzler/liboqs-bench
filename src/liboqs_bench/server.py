2fa63369e65929cddc36e78faa81edeccf0adfc7
Denise Kienzler <168895001+denisekienzler@users.noreply.github.com>
2026-09-03 16:50:48 +0200

    graceful server exit

--- src/liboqs_bench/server.py
+++ src/liboqs_bench/server.py
@@ -19,37 +19,42 @@ from .custom_exceptions import *
 
 def liboqs_bench_server(host, port):
 
-    # Check network address
-    ipaddress.ip_address(host)
-    if port not in range(1024,49151):
-        raise ValueError(f'{port} not in range of registered ports 1024-49151')
-    _logger.debug('Network address legitimate.')
+    try: # to exit gracefully when KeyboardInterrupt
 
-    with socket.socket() as s:
-        print(f'Starting unsecured server with endpoint at {host}:{port}...')
-        s.bind((host, port))
-        s.listen()
-        while True:
-            _logger.info('Accepting connections...')
-            conn,addr = s.accept()
-            _logger.info(f'Connected to by {addr}.')
-            with conn:
-                req_payload = conn.recv(1024)
-                _logger.debug(f'Request received by {addr}')
-                req_data = pickle.loads(req_payload)
-                _logger.debug('Deserialized request. Sending data to liboqs_bench function ...')
-                try:
-                    resp_data = liboqs_bench(req_data['alg'], *req_data['schemes'], iterations=req_data['iterations'])
-                    _logger.debug('Received data from liboqs_bench. Serializing and sending ...')
-                except UnsupportedAlgorithmError as a:
-                    _logger.warning('UnsupportedAlgorithmError')
-                    resp_data = a.args[0]
-                except UnsupportedSchemeError as c:
-                    _logger.warning('UnsupportedSchemeError')
-                    resp_data = c.args[0]
-                except NoSchemeProvidedError as p:
-                    _logger.warning('NoSchemeProvidedError')
-                    resp_data = p.args[0]
-                resp_payload = pickle.dumps(resp_data, pickle.HIGHEST_PROTOCOL)
-                conn.sendall(resp_payload)
-            _logger.info(f'Connection by {addr} closed.')
+        # Check network address
+        ipaddress.ip_address(host)
+        if port not in range(1024,49151):
+            raise ValueError(f'{port} not in range of registered ports 1024-49151')
+        _logger.debug('Network address legitimate.')
+
+        with socket.socket() as s:
+            print(f'Starting unsecured server with endpoint at {host}:{port}...')
+            s.bind((host, port))
+            s.listen()
+            while True:
+                _logger.info('Accepting connections...')
+                conn,addr = s.accept()
+                _logger.info(f'Connected to by {addr}.')
+                with conn:
+                    req_payload = conn.recv(1024)
+                    _logger.debug(f'Request received by {addr}')
+                    req_data = pickle.loads(req_payload)
+                    _logger.debug('Deserialized request. Sending data to liboqs_bench function ...')
+                    try:
+                        resp_data = liboqs_bench(req_data['alg'], *req_data['schemes'], iterations=req_data['iterations'])
+                        _logger.debug('Received data from liboqs_bench. Serializing and sending ...')
+                    except UnsupportedAlgorithmError as a:
+                        _logger.warning('UnsupportedAlgorithmError')
+                        resp_data = a.args[0]
+                    except UnsupportedSchemeError as c:
+                        _logger.warning('UnsupportedSchemeError')
+                        resp_data = c.args[0]
+                    except NoSchemeProvidedError as p:
+                        _logger.warning('NoSchemeProvidedError')
+                        resp_data = p.args[0]
+                    resp_payload = pickle.dumps(resp_data, pickle.HIGHEST_PROTOCOL)
+                    conn.sendall(resp_payload)
+                _logger.info(f'Connection by {addr} closed.')
+
+    except KeyboardInterrupt:
+        print(f'\nClosing unsecured server at {host}:{port}.')
